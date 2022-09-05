d63f0d19edafcb990fed770fd851f684595b973a
dailyzett <dailyzett@gmail.com>
2022-09-05 22:19:16 +0900

    테스트 조건 변경

--- src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -1,24 +1,20 @@
 package hello.advanced.trace.threadlocal;
 
-import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class FieldServiceTest {
 
-	private FieldService fieldService = new FieldService();
+	private final ThreadLocalService fieldService = new ThreadLocalService();
 
 	@Test
 	void field() {
 		log.info("main start");
-		Runnable userA = () -> {
-			fieldService.logic("userA");
-		};
-		Runnable userB = () -> {
-			fieldService.logic("userB");
-		};
-
+		Runnable userA = () -> fieldService.logic("userA");
+		Runnable userB = () -> fieldService.logic("userB");
+		
 		Thread threadA = new Thread(userA);
 		threadA.setName("thread-A");
 		Thread threadB = new Thread(userB);
